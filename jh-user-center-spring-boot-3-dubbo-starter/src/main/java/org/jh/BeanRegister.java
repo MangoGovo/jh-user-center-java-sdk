26a088d6e865adab887ebe3e9265012a0caf94c3
MangoGovo <1749100231@qq.com>
2025-08-07 21:29:56 +0800

    feat

---
+++ jh-user-center-spring-boot-3-dubbo-starter/src/main/java/org/jh/BeanRegister.java
@@ -0,0 +1,31 @@
+package org.jh;
+
+import jakarta.annotation.Resource;
+import org.apache.dubbo.common.constants.CommonConstants;
+import org.apache.dubbo.config.ReferenceConfig;
+import org.apache.dubbo.config.bootstrap.DubboBootstrap;
+import org.jh.api.UserCenterService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author MangoGovo
+ */
+@Configuration
+public class BeanRegister {
+    @Resource
+    private Properties properties;
+
+    @Bean
+    public UserCenterService userCenterService() {
+        DubboBootstrap bootstrap = DubboBootstrap.getInstance();
+        ReferenceConfig<UserCenterService> ref = new ReferenceConfig<>();
+        ref.setInterface(UserCenterService.class);
+        ref.setProtocol(CommonConstants.TRIPLE);
+        ref.setProxy(CommonConstants.NATIVE_STUB);
+        ref.setTimeout(3000);
+        ref.setUrl(properties.getGrpc());
+        bootstrap.reference(ref);
+        return ref.get();
+    }
+}
---
+++ jh-user-center-spring-boot-3-dubbo-starter/src/main/java/org/jh/Properties.java
@@ -0,0 +1,15 @@
+package org.jh;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author MangoGovo
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "user-center")
+public class Properties {
+    private String grpc;
+}
